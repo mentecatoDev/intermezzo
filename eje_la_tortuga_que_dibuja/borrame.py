c1b80e0d77fa5b13fd4886d70448e76d9d360b73
mentecatoDev <favila@iesromerovargas.com>
2020-04-06 14:25:16 +0100

    Deployed 6bcf63d with MkDocs version: 1.0.4

--- eje_la_tortuga_que_dibuja/borrame.py
+++ eje_la_tortuga_que_dibuja/borrame.py
@@ -1,64 +1,18 @@
-"""      turtle-example-suite:
-
-          tdemo_wikipedia3.py
-
-This example is
-inspired by the Wikipedia article on turtle
-graphics. (See example wikipedia1 for URLs)
-
-First we create (ne-1) (i.e. 35 in this
-example) copies of our first turtle p.
-Then we let them perform their steps in
-parallel.
-
-Followed by a complete undo().
-"""
-from turtle import Screen, Turtle, mainloop
-from time import perf_counter as clock, sleep
-
-def mn_eck(p, ne,sz):
-    turtlelist = [p]
-    #create ne-1 additional turtles
-    for i in range(1,ne):
-        q = p.clone()
-        q.rt(360.0/ne)
-        turtlelist.append(q)
-        p = q
-    for i in range(ne):
-        c = abs(ne/2.0-i)/(ne*.7)
-        # let those ne turtles make a step
-        # in parallel:
-        for t in turtlelist:
-            t.rt(360./ne)
-            t.pencolor(1-c,0,c)
-            t.fd(sz)
-
-def main():
-    s = Screen()
-    s.bgcolor("black")
-    p=Turtle()
-    p.speed(0)
-    p.hideturtle()
-    p.pencolor("red")
-    p.pensize(3)
-
-    s.tracer(36,0)
-
-    at = clock()
-    print(at)
-    mn_eck(p, 36, 19)
-
-    sleep(1)
-
-    while any(t.undobufferentries() for t in s.turtles()):
-        for t in s.turtles():
-            t.undo()
-    et = clock()
-    return "runtime: %.3f sec" % (et-at)
-
-
-if __name__ == '__main__':
-    msg = main()
-    print(msg)
-    mainloop()
-
+from turtle import *
+from math import sin, cos, pi
+r = 200
+inc = 2*pi/100
+t=0
+n=1.5
+speed(0)
+for i in range(100):
+    x1=r*sin(t)
+    y1=r*cos(t)
+    x2=r*sin(t+n)
+    y2=r*cos(t+n)
+    penup()
+    goto(x1,y1)
+    pendown()
+    goto(x2,y2)
+    t+=inc
+    
